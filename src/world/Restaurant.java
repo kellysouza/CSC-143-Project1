6b69d4d3ac9aeb47def789cb9be1fd08a145ec71
Kelly Souza <kellysouza99@gmail.com>
2020-04-25 15:02:28 -0700

    Restaurant and Supermarket tests passing

--- src/world/Restaurant.java
+++ src/world/Restaurant.java
@@ -5,26 +5,69 @@ import lib.Pair;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 public class Restaurant extends Store {
     public HashMap<String, Collection<Pair<String, Integer>>> recipes = new HashMap<>();
+
     public Restaurant(String location, String company) {
         super(location, company);
     }
 
     @Override
     public Collection<Pair<String, Integer>> purchase(Collection<Pair<String, Integer>> order) {
-        return null;
+        Collection<Pair<String, Integer>> purchase = new ArrayList<>();
+        for (Pair orderItem : order) {
+            int qtyAvail = getQuantityAvailable((String) orderItem.left);
+            if (qtyAvail > (int) orderItem.right) {
+                purchase.add(orderItem);
+                deductSupplies((String) orderItem.left, (Integer) orderItem.right);
+            } else if (qtyAvail == 0) {
+                //do nothing
+            } else {
+                purchase.add(new Pair<>((String) orderItem.left, qtyAvail));
+                deductSupplies((String) orderItem.left, qtyAvail);
+            }
+        }
+        return purchase;
+    }
+
+    public void deductSupplies(String orderItem, int qtyPurchased) {
+        Collection<Pair<String, Integer>> ingredients = recipes.get(orderItem);
+        for (Pair ingredient : ingredients) {
+            this.supplies.put((String) ingredient.left, this.supplies.get(ingredient.left) - qtyPurchased * ((int) ingredient.right));
+        }
     }
 
     @Override
     public Collection<Pair<String, Integer>> getProducts() {
-        return null;
+        Collection<Pair<String, Integer>> products = new ArrayList<>();
+        for (String recipe : recipes.keySet()) {
+            int quantityAvailable = getQuantityAvailable(recipe);
+            products.add(new Pair(recipe, quantityAvailable));
+        }
+        return products;
+    }
+
+    private int getQuantityAvailable(String recipe) {
+        if (!recipes.containsKey(recipe)) {
+            return 0;
+        }
+        int quantityAvailable = Integer.MAX_VALUE;
+        for (Pair ingredient : recipes.get(recipe)) {
+            if (!supplies.containsKey(ingredient.left)) {
+                quantityAvailable = 0;
+            } else {
+                int supplyQty = this.supplies.get(ingredient.left);
+                if (quantityAvailable > supplyQty / (int) ingredient.right) {
+                    quantityAvailable = supplyQty / (int) ingredient.right;
+                }
+            }
+        }
+        return quantityAvailable;
     }
 
     public void learnRecipe(String name, Collection<Pair<String, Integer>> ingredients) {
         recipes.put(name, ingredients);
     }
-
-    /* YOUR CODE HERE */
 }
--- src/world/Supermarket.java
+++ src/world/Supermarket.java
@@ -5,24 +5,52 @@ import lib.Pair;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
+/**
+ * Supermarket: Extends Store.
+ * The supermarket is a simple storefront which sells
+ * exactly what its inventory (supply) contains.
+ */
 public class Supermarket extends Store {
+
     public Supermarket(String location, String company) {
         super(location, company);
     }
 
+    /**
+     * getProducts(): Inherited from Store.
+     * <p>
+     * Hint: you can iterate over this.supplies.keySet() to get names and quantities.
+     *
+     * @return list of products (as pairs, name and quantity) that reflects the store inventory.
+     */
     @Override
     public Collection<Pair<String, Integer>> getProducts() {
         ArrayList<Pair<String, Integer>> productList = new ArrayList<>();
-        /* YOUR CODE HERE */
-
+        for (String name : this.supplies.keySet()) {
+            productList.add(new Pair(name, this.supplies.get(name)));
+        }
         return productList;
     }
 
-    /* YOUR CODE HERE */
     @Override
     public Collection<Pair<String, Integer>> purchase(Collection<Pair<String, Integer>> order) {
-        return null;
-    }
+        Collection<Pair<String, Integer>> itemsFulfilled = new ArrayList<>();
+        for (Pair pair : order) {
+            if (this.supplies.containsKey(pair.left)) {
+//                String itemOrdered = (String)pair.left;
+//                int quantityOrdered = (Integer)pair.right;
+//                int quantityInStock = this.supplies.get(itemOrdered);
+//                int quantity = quantityInStock >= quantityOrdered ? quantityOrdered : quantityInStock;
+//                this.supplies.put(itemOrdered, quantityInStock - quantity);
+//                itemsFulfilled.add(new Pair(pair.left, quantity));
 
+                int quantity = this.supplies.get(pair.left) >= (int) pair.right ? (int) pair.right : this.supplies.get(pair.left);
+                this.supplies.put((String) pair.left, this.supplies.get(pair.left) - quantity);
+                itemsFulfilled.add(new Pair(pair.left, quantity));
+            }
+        }
+        return itemsFulfilled;
+    }
 }
